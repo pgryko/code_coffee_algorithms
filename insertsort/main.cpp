9eddf569505719463443824437a4248256696179
Piotr Gryko <Piotr.Gryko@gmail.com>
2018-11-13 18:37:38 +0200

    Playing with cpp array class

--- insertsort/main.cpp
+++ insertsort/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <array>
 #include <gtest/gtest.h>
 //https://www.eriksmistad.no/getting-started-with-google-test-on-ubuntu
 
@@ -19,14 +20,37 @@ struct BankAccount
     }
 };
 
+// Input 5,4,2,1,6,3,1
+// Expected output 1,1,2,3,4,5,6
+
 TEST(AccountTest, BankAccountStartsEmpty)
 {
     BankAccount account;
-    EXPECT_EQ(0,account.balance);
+    std::array<int,7> a1 {5,4,2,1,6,3,1};
+    std::array<int,7> a2 {5,4,2,1,6,3,1};
+    EXPECT_EQ(a1,a2);
+}
+
+template < typename T, size_t N > void insertsort(std::array<T, N> * Array)
+{
+
+    std::cout << "Insertsort!, array size is " << Array->size() << std::endl;
+
+    std::cout << "Array contains " << std::endl;
+
+
+    for (const auto& s : *Array)
+    {
+        std::cout << " " << s << ", ";
+    }
+    std::cout <<  std::endl;
+
 }
 
 int main(int argc, char* argv[]) {
-//std::cout << "Hello, World!" << std::endl;
+std::cout << "Hello, World!" << std::endl;
+    std::array<int,7> a1 {5,4,2,1,6,3,1};
+    insertsort(&a1);
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
