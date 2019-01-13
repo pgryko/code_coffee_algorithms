cf7c04979e3da010630eea233463c33053ca2179
Piotr Gryko <piotr.gryko@gmail.com>
2019-01-14 03:52:31 +0800

    Got insert sort working for python and updated gitlab ci

--- src/insertsort/insertsort.py
+++ src/insertsort/insertsort.py
@@ -1,11 +1,18 @@
 def insert_sort(array):
 
-    for index, item in enumerate(array):
+    for i in range(1, len(array)):
+        j = i - 1
 
-        print(item)
+        key = array[i]
 
+        while key < array[j] and j > -1:
+            array[j+1] = array[j]
+            j = j -1
+        array[j + 1] = key
 
 if __name__ == '__main__':
-    array = [5, 4, 3, 2, 1, 0]
+    array = [8, 0, 3, 3, 5, 6]
 
     insert_sort(array)
+
+    print(array)
--- src/insertsort/main.cpp
+++ src/insertsort/main.cpp
@@ -17,21 +17,6 @@ void printarray(const std::array<T, N> *Array) {
 
 }
 
-//template<typename T, size_t N>
-//void insertsort(std::array<T, N> *Array) {
-//
-//    for (std::size_t i(1), j(0); i < Array->size(); i++)
-//    {
-//        T key = Array->at(i);
-//        j = i - 1 ;
-//        while( j != std::numeric_limits<std::size_t >::max() && Array->at(j) > key){
-//            (*Array)[j+1] = (*Array)[j];
-//            j = j - 1;
-//        };
-//        (*Array)[(j+1)] = key;
-//
-//    }
-//}
 
 TEST(InsertSortTest, SortArray1) {
 std::array<int, 7> a1{5, 4, 2, 1, 6, 3, 1};
