8a967f6060829fd5ef51176f63efceae148e0367
Piotr Gryko <piotr.gryko@gmail.com>
2019-12-11 17:00:08 +0000

    Got a horrible version of mergesort working for python

--- src/mergesort/mergesort.py
+++ src/mergesort/mergesort.py
@@ -1,22 +1,61 @@
-def merge_sort(array):
-    '''Sets off merge sort'''
+import copy
+
+class merge_sort(object):
+
+    def __init__(self, array):
+        '''Sets off merge sort'''
+        merge_sort.__merge_sort(array, 0, len(array) - 1)
+
+
+    @staticmethod
+    def merge(array,low,mid,high):
+        lhs = copy.deepcopy(array[low:mid+1])
+        rhs = copy.deepcopy(array[mid + 1 : high + 1])
+        print(str(low) + " " + str(mid) + " " + str(high))
+
+        print("Merging")
+
+        print("lhs")
+        print(lhs)
+        print("rhs")
+        print(rhs)
+
+        i = 0
+        j = 0
+        for k in range(low,high+1):
+            if  i < len(lhs) and j < len(rhs):
+
+                if lhs[i] <= rhs[j]:
+
+                    array[k] = lhs[i]
+                    i += 1
+                else:
+                    array[k] = rhs[j]
+                    j += 1
+            else:
+                if i < len(lhs):
+                    array[k] = lhs[i]
+                    i += 1
+                else:
+                    array[k] = rhs[j]
+
 
     def __merge_sort(array,low,high):
 
-        mid = (low + high)/2
-        pass
+        mid = int((low + high)/2)
 
-    mid = len(array)/2
+        if mid < high:
+            merge_sort.__merge_sort(array,low,mid)
+            merge_sort.__merge_sort(array,mid+1,high)
+            merge_sort.merge(array,low,mid,high)
 
-    __merge_sort(array,0,mid,)
-    pass
 
-def merge(array,low,mid,high):
-    pass
+
 
 if __name__ == '__main__':
     array = [8, 0, 3, 3, 5, 6]
 
     merge_sort(array)
+
 
     print(array)
