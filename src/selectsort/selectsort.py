e0a01a2a4b67679d05a7a642ffdcc72ccc2a17d7
Piotr Gryko <piotr.gryko@gmail.com>
2020-11-16 17:43:59 +0200

    Updated README and some cleanup

--- src/selectsort/selectsort.py
+++ src/selectsort/selectsort.py
@@ -1,25 +1,21 @@
 def select_sort(array):
+    def swap_values(array, index, min_index):
 
-	def swap_values(array, index, min_index):
+        temp_val = array[min_index]
+        array[min_index] = array[index]
+        array[index] = temp_val
 
-		temp_val = array[min_index]
-		array[min_index] = array[index]
-		array[index] = temp_val
+    for i in range(0, len(array)):
+        j = i + 1
+        # Second loop to find min value
 
+        min_index = i
+        while j < len(array):
+            if array[min_index] > array[j]:
+                min_index = j
+            j = j + 1
 
-	for i in range(0,len(array)):
-		j = i + 1
-		# Second loop to find min value
-		
-		min_index = i
-		while j < len(array):
-			if array[min_index] > array[j]:
-				min_index = j
-			j = j + 1
-
-		swap_values(array,i,min_index)
-
-
+        swap_values(array, i, min_index)
 
 
 if __name__ == '__main__':
