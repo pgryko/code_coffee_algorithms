88d1f667295f3f3b6d7670eee6102190769b295d
Piotr Gryko <piotr.gryko@gmail.com>
2020-11-23 21:22:39 +0200

    WIP

--- src/heapsort/heapsort.py
+++ src/heapsort/heapsort.py
@@ -1,4 +1,4 @@
-def MaxHeapify(array, size, i):
+def MaxHeapify(array, i: int = 0, size=None):
     '''
 
     :param array: Array to be sorted
@@ -12,36 +12,52 @@ def MaxHeapify(array, size, i):
     left = 2 * i + 1
     right = 2 * i + 2
 
+    if size is None:
+        size = len(array)
+
     if left < size and array[left] > array[i]:
         largest = left
 
-    if right < size and array[right] > array[i]:
+    if right < size and array[right] > array[largest]:
         largest = right
 
     # If largest is not root
     if largest != i:
         array[largest], array[i] = array[i], array[largest]
-        MaxHeapify(array, i, 0)
+        # Ensure that sub tree maintains maxheap via recursive call
+        MaxHeapify(array, largest, size)
 
 
-def HeapSort(array):
-    if len(array) < 2:
-        return
-
+def BuildHeap(array):
     # Build heap (re-arrange array)
     # range(start, stop, step)
     # operator // forces integer division (python 3)
-    for i in range(len(array) // 2 - 1, -1, -1):
-        MaxHeapify(array, len(array), i)
+    for i in range((len(array) - 1) // 2, -1, -1):
+        MaxHeapify(array, i)
+
+
+def HeapSort(array):
+    if len(array) < 1:
+        return
+
+    # # Build heap (re-arrange array)
+    # # range(start, stop, step)
+    # # operator // forces integer division (python 3)
+    for i in range((len(array) - 1) // 2, -1, -1):
+        MaxHeapify(array, i)
+
+    # We now have a max heap, one by one pull out the maximum element
+    # and add it to the end of the array, and sort the remainder
+    size = len(array)
 
     for i in range(len(array) - 1, -1, -1):
         array[0], array[i] = array[i], array[0]
-        MaxHeapify(array, len(array), i)
+
+        size = size - 1
+        MaxHeapify(array, 0, size)
 
 
 if __name__ == '__main__':
     array_in = [8, 0, 3, 3, 5, 6, 7]
 
     HeapSort(array_in)
-
-    print(array_in)
--- src/heapsort/test_heapsort.py
+++ src/heapsort/test_heapsort.py
@@ -1,34 +1,72 @@
-# import unittest
-# from mergesort import merge_sort
+import unittest
+from heapsort import MaxHeapify, BuildHeap, HeapSort
 
 
-# class TestMerge(unittest.TestCase):
+class TestMaxHeapify(unittest.TestCase):
+    '''
+    Test the building of a maxheap from an array.
+    A max heap is a loosely ordered datastructure, requiring that
+    the top (root) node has a higher maximum than its leaves
 
-#     def test_merge_even_A(self):
-#         array_in = [3,4]
-#         merge_sort.merge(array_in,0,0,1)
-#         self.assertEqual(array_in, [3,4])
+    '''
 
+    def test_2_elem(self):
 
-#     def test_merge_even_B(self):
-#         array_in = [4,3]
-#         merge_sort.merge(array_in,0,0,1)
-#         self.assertEqual(array_in, [3,4])
+        array_in = [3,4]
+        MaxHeapify(array_in)
+        self.assertEqual(array_in, [4,3])
 
-#     def test_merge_even_B(self):
-#         array_in = [4,4]
-#         merge_sort.merge(array_in,0,0,1)
-#         self.assertEqual(array_in, [4,4])
+        array_in = [4,3]
+        MaxHeapify(array_in)
+        self.assertEqual(array_in, [4,3])
 
-#     def test_merge_odd_A(self):
-#         array_in = [1,4,2]
-#         merge_sort.merge(array_in,0,1,2)
-#         self.assertEqual(array_in, [1,2,4])
+    def test_3_elem(self):
 
-#     def test_merge_odd_A(self):
-#         array_in = [4,4,2]
-#         merge_sort.merge(array_in,0,1,2)
-#         self.assertEqual(array_in, [2,4,4])
+        array_in = [3,5,4]
+        MaxHeapify(array_in)
+        self.assertEqual(array_in, [5,3,4])
+
+        array_in = [4,3,5]
+        MaxHeapify(array_in)
+        self.assertEqual(array_in, [5,3,4])
+
+    def test_5_elem(self):
+
+        array_in = [3,5,4,7,2]
+        MaxHeapify(array_in,1)
+        self.assertEqual(array_in,[3,7,4,5,2])
+
+        MaxHeapify(array_in)
+        self.assertEqual(array_in, [7,5,4,3,2])
+
+    def test_7_elem(self):
+
+        array_in = [8, 0, 3, 3, 5, 6, 7]
+        MaxHeapify(array_in,1)
+        self.assertEqual(array_in,[8,5,3,3,0,6,7])
+
+    def test_size(self):
+        '''Test passing in a smaller size
+        '''
+        array_in = [3, 5, 7, 3, 0, 6, 8]
+        MaxHeapify(array_in, 2)
+        self.assertEqual(array_in, [3, 5, 8, 3, 0, 6, 7])
+
+        array_in = [3, 5, 7, 3, 0, 6, 8, 9]
+        MaxHeapify(array_in, 2)
+        self.assertEqual(array_in, [3, 5, 8, 3, 0, 6, 7, 9])
+
+        array_in = [3, 5, 7, 3, 0, 6, 8, 9]
+        MaxHeapify(array_in, 2, len(array_in) - 1)
+        self.assertListEqual(array_in,[3, 5, 8, 3, 0, 6, 7, 9])
+
+def TestBuildHeap(self):
+
+    def test_build_heap(self):
+
+        array_in = [8, 0, 3, 3, 5, 6, 7]
+        BuildHeap(array_in)
+        self.assertEqual(array_in,[8,5,3,3,0,6,7])
 
 
 # class TestMergeSort(unittest.TestCase):
