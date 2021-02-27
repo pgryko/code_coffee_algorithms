d04044afd417c1c782b51977f2ed923e69060127
Piotr Gryko <piotr.gryko@gmail.com>
2021-02-27 22:21:17 +0200

    Working on pointers

--- src/data_structures/linkedlists/linkedlist.hpp
+++ src/data_structures/linkedlists/linkedlist.hpp
@@ -1,98 +1,92 @@
 // Custom example of implementing a linked list using C++
-//https://www.geeksforgeeks.org/implementing-iterator-pattern-of-a-single-linked-list/
-//https://gist.github.com/jeetsukumaran/307264
+// https://www.geeksforgeeks.org/implementing-iterator-pattern-of-a-single-linked-list/
+// https://gist.github.com/jeetsukumaran/307264
 #include <iostream>
 #include <sstream>
 
-template<typename T>
+template <typename T>
 class Node {
-// We could have set LinkedList as friend class,
-// but as we don't expect to access the nodes directly
-// they are private inside Linked list
-    Node *pNext;
+  // We could have set LinkedList as friend class,
+  // but as we don't expect to access the nodes directly
+  // they are private inside Linked list
+  Node* pNext;
 
-    //Forward declaration
-    class LinkedList;
-    // Allow likedlist direct access to node *pNext pointer
-    friend class LinkedList;
+  // Forward declaration
+  class LinkedList;
+  // Allow likedlist direct access to node *pNext pointer
+  friend class LinkedList;
 
-    template<class U> friend std::ostream& operator<<(std::ostream& out, const Node<T>& node);
-    template<class U> friend std::istream& operator>>(std::istream& in, Node<T>& node);
+  template <class U>
+  friend std::ostream& operator<<(std::ostream& out, const Node<T>& node);
+  template <class U>
+  friend std::istream& operator>>(std::istream& in, Node<T>& node);
 
-public:
-    T data;
+ public:
+  T data;
 
-    Node(T data) : data(data), pNext(nullptr) {}
+  Node(T data) : data(data), pNext(nullptr) {}
 
-    Node(T data, Node *pNext) {
-        data = data;
-        pNext = pNext;
-    }
+  Node(T data, Node* pNext) {
+    data = data;
+    pNext = pNext;
+  }
 
-    T getData() {
-        return data;
-    }
+  T getData() { return data; }
 
+  void setData(T data) { data = data; }
 
+  Node* getNext() { return pNext; }
 
+  void setNext(Node* node) { pNext = node; }
 };
 
-template<typename T> std::ostream& operator<<(std::ostream& out, const Node<T>& node)
-{
-    std::cout << node.data << std::endl;
-
-    return out << node.data;
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const Node<T>& node) {
+  return out << node.data;
 }
 
-template<typename T> std::istream& operator>>(std::istream& in, Node<T>& node)
-{
-    return in >> node.data;
+template <typename T>
+std::istream& operator>>(std::istream& in, Node<T>& node) {
+  return in >> node.data;
 }
 
-template<typename T>
+template <typename T>
 class LinkedList {
-// Members are private by default
-// Forward declaration
-    class Node;
+  // Members are private by default
+  // Forward declaration
+  class Node;
 
-public:
-//    Function should not throw
-    LinkedList<T>() noexcept {
+ public:
+  //    Function should not throw
+  LinkedList<T>() noexcept { Node* head_prt(nullptr); }
 
-        Node *head_prt(nullptr);
+  // std::iterator is being depreciated
+  // https://stackoverflow.com/questions/37031805/preparation-for-stditerator-being-deprecated/38103394
+  //    class Iterator;
+  //
+  //    Iterator begin(){
+  //    return Iterator(head_prt);
+  //    }
+  //
+  //    Iterator end(){
+  //    return Iterator(nullprt);
+  //    }
 
-    }
+  //    void push_back(T data);
 
+  void PrintList();
 
-// std::iterator is being depreciated https://stackoverflow.com/questions/37031805/preparation-for-stditerator-being-deprecated/38103394
-//    class Iterator;
-//
-//    Iterator begin(){
-//    return Iterator(head_prt);
-//    }
-//
-//    Iterator end(){
-//    return Iterator(nullprt);
-//    }
-
-//    void push_back(T data);
-
-    void PrintList();
-
-    // Return by reference so that it can be used in
-    // left hand side of the assignment expression
-    Node *&GetHeadNode() {
-        return this->head_prt;
-    }
-
+  // Return by reference so that it can be used in
+  // left hand side of the assignment expression
+  Node*& GetHeadNode() { return this->head_prt; }
 };
 
-template<typename T>
+template <typename T>
 void LinkedList<T>::PrintList() {
-    Node *pCrawler = GetHeadNode();
+  Node* pCrawler = GetHeadNode();
 
-    while (pCrawler) {
-        std::cout << pCrawler->data << " ";
-        pCrawler = pCrawler->pNext;
-    }
+  while (pCrawler) {
+    std::cout << pCrawler->data << " ";
+    pCrawler = pCrawler->pNext;
+  }
 }
--- src/data_structures/linkedlists/main.cpp
+++ src/data_structures/linkedlists/main.cpp
@@ -6,13 +6,32 @@
 
 using namespace std;
 
+TEST(Node, TestConstructor) {
+    Node mynode2("72");
+    EXPECT_EQ("72", mynode2.getData());
+    EXPECT_EQ(nullptr, mynode2.getNext());
+
+    Node mynode1("60", &mynode2 );
+    EXPECT_EQ("60", mynode1.getData());
+    Node<const char *> *prt = &mynode2;
+    cout << &mynode1 << endl;
+    cout << &mynode2 << endl;
+    EXPECT_EQ(prt, mynode1.getNext());
+//    EXPECT_EQ("72", mynode1.getNext()->getData());
+}
+
 int main(int argc, char *argv[]) {
+    Node mynode2("72", nullptr);
 
-    Node mynode(111);
-
-    cout << mynode.getData() << endl;
-
+    Node mynode1("60", &mynode2 );
+    cout << &mynode1 << endl;
+    cout << mynode1.getNext() << endl;
+    cout << nullptr << endl;
+    cout << &mynode2 << endl;
+    cout << mynode2.getNext() << endl;
 
     return 0;
+//    testing::InitGoogleTest(&argc, argv);
+//
 //    return RUN_ALL_TESTS();
 }
