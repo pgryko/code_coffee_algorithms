da848ec36313ca5c906cae26caa43f59f686027c
Piotr Gryko <piotr.gryko@gmail.com>
2023-02-05 15:47:30 +0100

    feat: added solution for leetcode nearest matrix

---
+++ src/problems/medium/matrix_nearest_value/matrix_nearest_value.py
@@ -0,0 +1,54 @@
+"""
+Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+
+The distance between two adjacent cells is 1.
+
+Example 1:
+
+Input: mat = [[0,0,0],[0,1,0],[0,0,0]]
+Output: [[0,0,0],[0,1,0],[0,0,0]]
+
+Example 2:
+
+Input: mat = [[0,0,0],[0,1,0],[1,1,1]]
+Output: [[0,0,0],[0,1,0],[1,2,1]]
+"""
+from typing import List, Tuple
+
+
+def is_valid(x_len: int, y_len: int, pair: Tuple):
+    return 0 <= pair[0] < x_len and 0 <= pair[1] < y_len
+
+
+def breath_first_search(matrix=List[List]) -> List[List]:
+    x_len = len(matrix)
+    y_len = len(matrix[0])
+
+    dist = [[-1 for _ in range(0, y_len)] for _ in range(0, x_len)]
+
+    queue = []
+
+    # Loop through the matrix and find the source nodes
+    # We will be searching from nodes with value zero to nodes == 1
+    for x in range(0, x_len):
+        for y in range(0, y_len):
+            if matrix[x][y] == 0:
+                dist[x][y] = 0
+                queue.append((x, y))
+
+    x_adj = [-1, 0, 1, 0]
+    y_adj = [0, -1, 0, 1]
+
+    while len(queue) > 0:
+        node = queue.pop(0)
+
+        for i in range(0, 4):
+            adj_node = (node[0] + x_adj[i], node[1] + y_adj[i])
+            if (
+                is_valid(x_len=x_len, y_len=y_len, pair=adj_node)
+                and dist[adj_node[0]][adj_node[1]] == -1
+            ):
+                dist[adj_node[0]][adj_node[1]] = dist[node[0]][node[1]] + 1
+                queue.append(adj_node)
+
+    return dist
---
+++ src/problems/medium/matrix_nearest_value/test_matrix_nearest_value.py
@@ -0,0 +1,43 @@
+import unittest
+from matrix_nearest_value import breath_first_search, is_valid
+
+
+class TestMatrixNearestValue(unittest.TestCase):
+    def test_is_valid_true(self):
+        self.assertTrue(is_valid(x_len=3, y_len=3, pair=(0, 0)))
+        self.assertTrue(is_valid(x_len=3, y_len=3, pair=(0, 1)))
+        self.assertTrue(is_valid(x_len=3, y_len=3, pair=(1, 0)))
+        self.assertTrue(is_valid(x_len=3, y_len=3, pair=(1, 1)))
+        self.assertTrue(is_valid(x_len=3, y_len=3, pair=(2, 2)))
+
+    def test_is_valid_false(self):
+        self.assertFalse(is_valid(x_len=3, y_len=3, pair=(-1, 0)))
+        self.assertFalse(is_valid(x_len=3, y_len=3, pair=(0, -1)))
+        self.assertFalse(is_valid(x_len=3, y_len=3, pair=(-1, -1)))
+        self.assertFalse(is_valid(x_len=3, y_len=3, pair=(3, 2)))
+        self.assertFalse(is_valid(x_len=3, y_len=3, pair=(2, 3)))
+        self.assertFalse(is_valid(x_len=3, y_len=3, pair=(3, 3)))
+
+    def test_all_zero(self):
+        self.assertEqual(
+            breath_first_search([[0, 0, 0], [0, 0, 0], [0, 0, 0]]),
+            [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+        )
+
+    def test_all_one(self):
+        self.assertEqual(
+            breath_first_search([[1, 1, 1], [1, 1, 1], [1, 1, 1]]),
+            [[-1, -1, -1], [-1, -1, -1], [-1, -1, -1]],
+        )
+
+    def test_single(self):
+        self.assertEqual(
+            breath_first_search([[0, 0, 0], [0, 1, 0], [0, 0, 0]]),
+            [[0, 0, 0], [0, 1, 0], [0, 0, 0]],
+        )
+
+    def test_multiple(self):
+        self.assertEqual(
+            breath_first_search([[0, 0, 0], [0, 1, 0], [1, 1, 1]]),
+            [[0, 0, 0], [0, 1, 0], [1, 2, 1]],
+        )
