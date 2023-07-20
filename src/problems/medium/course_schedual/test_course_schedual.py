d1c511c17405ee71a030cd4e74244df0f23635b6
Piotr Gryko <piotr.gryko@gmail.com>
2023-07-20 15:40:03 +0200

    Added course schedual and added sonarcloud

---
+++ src/problems/medium/course_schedual/test_course_schedual.py
@@ -0,0 +1,35 @@
+import unittest
+
+from course_schedual import canFinish
+
+
+class CanFinishTestCase(unittest.TestCase):
+    def test_canFinish_withValidCourseOrder_shouldReturnTrue(self):
+        numCourses = 4
+        prerequisites = [[1, 0], [2, 1], [3, 2]]
+        self.assertTrue(canFinish(numCourses, prerequisites))
+
+    def test_canFinish_withValidCourseOrder_shouldReturnTrue_2(self):
+        numCourses = 4
+        prerequisites = [[1, 0], [2, 0], [3, 2], [3, 1]]
+        self.assertTrue(canFinish(numCourses, prerequisites))
+
+    def test_canFinish_withInvalidCourseOrder_shouldReturnFalse(self):
+        numCourses = 4
+        prerequisites = [[1, 0], [0, 1], [3, 2]]
+        self.assertFalse(canFinish(numCourses, prerequisites))
+
+    def test_canFinish_withNoPrerequisites_shouldReturnTrue(self):
+        numCourses = 3
+        prerequisites = []
+        self.assertTrue(canFinish(numCourses, prerequisites))
+
+    def test_canFinish_withSingleCourse_shouldReturnTrue(self):
+        numCourses = 1
+        prerequisites = []
+        self.assertTrue(canFinish(numCourses, prerequisites))
+
+    def test_canFinish_withCircularDependency_shouldReturnFalse(self):
+        numCourses = 3
+        prerequisites = [[0, 1], [1, 2], [2, 0]]
+        self.assertFalse(canFinish(numCourses, prerequisites))
